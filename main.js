babdf4accae9a0aabe9f04cbb5b83f6bd185f8fa
Mughees39 <mugheesahmad39@gmail.com>
2025-07-04 13:55:54 +0500

    contact form added
    Signed-off-by: Mughees39 <mugheesahmad39@gmail.com>

--- main.js
+++ main.js
@@ -57,14 +57,40 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // Form submission (prevent default for demo)
-  const contactForm = document.querySelector(".contact-form");
-  if (contactForm) {
-    contactForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      alert(
-        "Form submission would be handled here. In a real implementation, this would send data to a server."
-      );
-      contactForm.reset();
-    });
-  }
+  //const contactForm = document.querySelector(".contact-form");
+  //if (contactForm) {
+    //contactForm.addEventListener("submit", (e) => {
+      //e.preventDefault();
+      //alert(
+        //"Form submission would be handled here. In a real implementation, this would send data to a server."
+      //);
+      //contactForm.reset();
+    //});
+  //}
+
+  emailjs.init("Pn9uaGxw2hXs-llnG"); // 🔁 Replace this with your actual public key
+
+// Form submission handler
+
+  const form = document.getElementById("contact-form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_u5skyro", "template_p09eknt ", form)
+      .then(() => {
+        alert("Message sent successfully! ✅");
+        form.reset();
+      })
+      .catch((error) => {
+        console.error("EmailJS Error:", error);
+        alert("Failed to send message ❌");
+      });
+  });
+
 });
+
+//service_u5skyro            service id
+//0For34Sn-JUxmH7_L8np6      private key
+//Pn9uaGxw2hXs-llnG          public key
+//template_p09eknt           template id
